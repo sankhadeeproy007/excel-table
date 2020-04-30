2d2fd5df1c4ce617fdabb3ec4accf4470d793fe8
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2020-04-30 17:29:37 +0530

    chore(): eslint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,6 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'object-curly-newline': ['error', { consistent: true }],
   },
 };
